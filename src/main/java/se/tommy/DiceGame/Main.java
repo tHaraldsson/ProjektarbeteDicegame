a1e30df3aed34085b3ccf3d3b08d3b038719addc
tHaraldsson <tommy.haraldsson@stud.sti.se>
2024-09-19 18:45:59 +0200

    first commit

--- src/main/java/se/tommy/DiceGame/Main.java
+++ src/main/java/se/tommy/DiceGame/Main.java
@@ -1,4 +1,92 @@
 package se.tommy.DiceGame;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+        Player player = new Player();
+
+
+        System.out.println("--------Välkommen till Dicegame!!---------");
+        System.out.println("Ange namn för spelare 1");
+        player.setNamePlayer1(scanner.nextLine());
+        System.out.println("Ange namn för spelare 2");
+        player.setNamePlayer2(scanner.nextLine());
+        System.out.println("\nSpelare1: " + player.getNamePlayer1() + "\n" + "Spelare2: " + player.getNamePlayer2());
+        System.out.println("\nNamnen valda. Nu startar spelet");
+
+        boolean continueGame = true;
+        while (continueGame) {
+            //SPELARE 1 BÖRJAR
+            System.out.println("\n" + player.getNamePlayer1() + " börjar spelet, tryck enter för att kasta tärningen");
+            scanner.nextLine();
+            int roll1 = random.nextInt(6) + 1;
+            System.out.println(player.getNamePlayer1() + " kastade: " + roll1);
+            System.out.println("Tryck enter igen för att kasta tärningen");
+            scanner.nextLine();
+            int roll2 = random.nextInt(6) + 1;
+            System.out.println(player.getNamePlayer1() + " kastade: " + roll2);
+            player.setScorePlayer1(diceSum(roll1, roll2));
+            System.out.println(player.getNamePlayer1() + " fick " + player.getScorePlayer1() + " poäng");
+
+            //SPELARE 2 BÖRJAR
+            System.out.println("\nNu är det " + player.getNamePlayer2() + "s tur, tryck enter för att kasta tärningen");
+            scanner.nextLine();
+            int roll3 = random.nextInt(6) + 1;
+            System.out.println(player.getNamePlayer2() + " kastade: " + roll3);
+            System.out.println("Tryck enter igen för att kasta tärningen");
+            scanner.nextLine();
+            int roll4 = random.nextInt(6) + 1;
+            System.out.println(player.getNamePlayer2() + " kastade: " + roll4);
+            player.setScorePlayer2(diceSum(roll3, roll4));
+            System.out.println(player.getNamePlayer2() + " fick " + player.getScorePlayer2() + " poäng");
+            System.out.println("\n" + player.getNamePlayer1() + " fick " + player.getScorePlayer1() + " poäng");
+
+            //IFSATS FÖR ATT SE VEM VINNAREN ÄR
+            if (player.getScorePlayer1() > player.getScorePlayer2()) {
+                System.out.println("\nVinnaren är: " + player.getNamePlayer1() + " med " + player.getScorePlayer1() + " poäng");
+            } else if (player.getScorePlayer2() > player.getScorePlayer1()) {
+                System.out.println("\nVinnaren är: " + player.getNamePlayer2());
+            } else if (player.getScorePlayer1() == player.getScorePlayer2()) {
+                System.out.println("\nDet blev oavgjort!!!");
+            }
+
+            boolean continueAsking = true;
+            while (continueAsking) {
+                System.out.println("\nvill du fortsätta spela? Y/N");
+                String answer = scanner.nextLine();
+
+                if (answer.equalsIgnoreCase("y")) {
+                    continueGame = true;
+                    continueAsking = false;
+                } else if (answer.equalsIgnoreCase("n")) {
+                    continueGame = false;
+                    continueAsking = false;
+                    System.out.println("Game Over!!!");
+                } else {
+                    System.out.println("Ogiltig inmatning, försök igen.");
+                    continueAsking = true;
+                }
+            }
+        }
+
+
+    }
+
+    public static int diceSum(int roll1, int roll2) {
+        int sum = roll1 + roll2;
+        return sum;
+    }
+//    // LEADERBOARD
+//    public static int calculateTotalScore() {
+//        int total = 0;
+//        //iter
+//        for (Player product1 : ) {
+//            total += product1.getScorePlayer1();
+//        }
+//        return total;
+//    }
 }
--- src/main/java/se/tommy/DiceGame/Player.java
+++ src/main/java/se/tommy/DiceGame/Player.java
@@ -1,4 +1,45 @@
 package se.tommy.DiceGame;
 
-public class player1 {
+public class Player {
+private String namePlayer1;
+private String namePlayer2;
+private int scorePlayer1;
+private int scorePlayer2;
+
+
+    public String getNamePlayer1() {
+        return namePlayer1;
+    }
+
+    public String getNamePlayer2() {
+        return namePlayer2;
+    }
+
+    public void setNamePlayer1(String namePlayer1) {
+        this.namePlayer1 = namePlayer1;
+    }
+
+    public void setNamePlayer2(String namePlayer2) {
+        this.namePlayer2 = namePlayer2;
+    }
+
+    public int getScorePlayer1() {
+        return scorePlayer1;
+    }
+
+    public void setScorePlayer1(int scorePlayer1) {
+        this.scorePlayer1 = scorePlayer1;
+    }
+    public int getScorePlayer2() {
+        return scorePlayer2;
+    }
+
+    public void setScorePlayer2(int scorePlayer2) {
+        this.scorePlayer2 = scorePlayer2;
+    }
+
+
 }
+
+
+
